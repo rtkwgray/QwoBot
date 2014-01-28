2053d041bcd017fb137ee5c40b0543a6d9513741
SL5R0 <warren@warren-gray.com>
2014-01-27 19:26:22 -0500

    - Twitter Plugin now shows the entire follow list when following or unfollowing users.

--- src/main/java/com/sl5r0/qwobot/plugins/twitter/TwitterState.java
+++ src/main/java/com/sl5r0/qwobot/plugins/twitter/TwitterState.java
@@ -1,24 +1,22 @@
 package com.sl5r0.qwobot.plugins.twitter;
 
-import com.google.common.collect.ImmutableSet;
 import com.google.common.primitives.Longs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import twitter4j.FilterQuery;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterStream;
+import twitter4j.*;
 
+import java.util.Map;
 import java.util.Set;
 
-import static com.google.common.collect.Sets.newHashSet;
+import static com.google.api.client.util.Maps.newHashMap;
+import static com.google.common.collect.ImmutableSet.copyOf;
 import static org.pircbotx.Colors.BLUE;
 
 public class TwitterState {
     private static final Logger log = LoggerFactory.getLogger(TwitterState.class);
     private final Twitter twitter;
     private final TwitterStream twitterStream;
-    private final Set<Long> follows = newHashSet();
+    private final Map<Long, String> follows = newHashMap();
     private boolean showingRetweets = false;
     private boolean showingReplies = false;
     private String tweetColor = BLUE;
@@ -30,14 +28,16 @@ public class TwitterState {
     }
 
     void follow(String handle) throws TwitterException {
-        follows.add(getTwitterUserIdForHandle(handle));
-        log.info("Followed user: " + handle);
+        final User user = getTwitterUserFromHandle(handle);
+        follows.put(user.getId(), user.getName());
+        log.info("Followed user: " + user.getName());
         listen();
     }
 
     void unfollow(String handle) throws TwitterException {
-        follows.remove(getTwitterUserIdForHandle(handle));
-        log.info("Unfollowed user: " + handle);
+        final User user = getTwitterUserFromHandle(handle);
+        follows.remove(user.getId());
+        log.info("Unfollowed user: " + user.getName());
         listen();
     }
 
@@ -65,17 +65,17 @@ public class TwitterState {
         this.tweetColor = tweetColor;
     }
 
-    Set<Long> getFollows() {
-        return ImmutableSet.copyOf(follows);
+    Set<String> getFollowHandles() {
+        return copyOf(follows.values());
     }
 
     private void listen() {
         if (!follows.isEmpty()) {
-            twitterStream.filter(new FilterQuery(Longs.toArray(follows)));
+            twitterStream.filter(new FilterQuery(Longs.toArray(follows.keySet())));
         }
     }
 
-    private long getTwitterUserIdForHandle(String handle) throws TwitterException {
-        return twitter.showUser(handle).getId();
+    private User getTwitterUserFromHandle(String handle) throws TwitterException {
+        return twitter.showUser(handle);
     }
 }
