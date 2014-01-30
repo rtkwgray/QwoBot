48658e0e33448299a311164e6ae6d696751db8d4
Warren Gray <warren@warren-gray.com>
2014-01-30 17:12:14 -0500

    -Updated configuration loading code to fail more gracefully if the config file can't be found.

--- src/main/java/com/sl5r0/qwobot/core/BotConfiguration.java
+++ src/main/java/com/sl5r0/qwobot/core/BotConfiguration.java
@@ -5,23 +5,23 @@ import org.apache.commons.configuration.XMLConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.util.Iterator;
 
 public class BotConfiguration extends XMLConfiguration {
-    private static final String CONFIG_FILE_PROPERTY = "qwobot.config.file";
-    private static final String DEFAULT_CONFIG_FILE = "qwobot.xml";
-    private static final String CONFIGURATION_FILE = System.getProperty(CONFIG_FILE_PROPERTY, DEFAULT_CONFIG_FILE);
     private static final Logger log = LoggerFactory.getLogger(BotConfiguration.class);
 
-    public BotConfiguration() throws ConfigurationException {
-        super(CONFIGURATION_FILE);
-        log.info("Loaded configuration from " + CONFIGURATION_FILE);
-        if (log.isDebugEnabled()) {
-            final Iterator<String> configurationKeys = getKeys();
-            while (configurationKeys.hasNext()) {
-                final String key = configurationKeys.next();
-                log.debug(key + " => " + getString(key));
-            }
+    public BotConfiguration(File configFile) throws ConfigurationException {
+        super(configFile);
+        if (!configFile.exists()) {
+            throw new ConfigurationException("Could not load configuration from " + configFile.getAbsolutePath());
+        }
+
+        log.info("Loaded configuration from " + configFile.getAbsolutePath());
+        final Iterator<String> configurationKeys = getKeys();
+        while (configurationKeys.hasNext()) {
+            final String key = configurationKeys.next();
+            log.info(key + " => " + getString(key));
         }
     }
 }
