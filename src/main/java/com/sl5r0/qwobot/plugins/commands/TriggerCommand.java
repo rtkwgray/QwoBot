871fb404198409673fd7b5296c5df2149beea0a3
Warren Gray <warren@warren-gray.com>
2014-01-30 09:27:02 -0500

    - Added unit tests for ParameterTriggerCommand. - Did a bit of refactoring about the way that ParameterTriggerCommand handles removal of the trigger from the arguments list.

--- src/main/java/com/sl5r0/qwobot/plugins/commands/TriggerCommand.java
+++ src/main/java/com/sl5r0/qwobot/plugins/commands/TriggerCommand.java
@@ -2,6 +2,9 @@ package com.sl5r0.qwobot.plugins.commands;
 
 import org.pircbotx.hooks.events.MessageEvent;
 
+/**
+ * A Command that is executed when the specified trigger is found in a message.
+ */
 public abstract class TriggerCommand extends MessageCommand {
     private final String trigger;
 
@@ -18,6 +21,11 @@ public abstract class TriggerCommand extends MessageCommand {
         if (event.getMessage().startsWith(trigger)) {
             triggered(event);
         }
+    }
+
+    @Override
+    public String getHelp() {
+        return trigger;
     }
 
     protected abstract void triggered(MessageEvent event);
