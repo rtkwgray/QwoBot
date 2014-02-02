88e6ee6b894e70e1b3f0964e9363e4d2ea205038
SL5R0 <warren@warren-gray.com>
2014-02-02 11:25:46 -0500

    - Updated format/color class to allow for multiple color formats through a unified API.

--- src/main/java/com/sl5r0/qwobot/core/Format.java
+++ src/main/java/com/sl5r0/qwobot/core/Format.java
@@ -1,6 +1,13 @@
 package com.sl5r0.qwobot.core;
 
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
 import org.pircbotx.Colors;
+
+import java.util.List;
+import java.util.Queue;
+
+import static com.google.common.collect.Queues.newArrayDeque;
 
 /**
  * Simply an enum to simplify use of the PircBotX Colors class.
@@ -25,20 +32,52 @@ public enum Format {
     BLUE(Colors.BLUE),
     MAGENTA(Colors.MAGENTA),
     DARK_GRAY(Colors.DARK_GRAY),
-    LIGHT_GRAY(Colors.LIGHT_GRAY);
+    LIGHT_GRAY(Colors.LIGHT_GRAY),
+    RAINBOW(ImmutableList.of(Colors.PURPLE, Colors.BLUE, Colors.CYAN, Colors.GREEN, Colors.YELLOW, Colors.RED)),
+    FIRE(ImmutableList.of(Colors.YELLOW, Colors.RED)),
+    ICE(ImmutableList.of(Colors.BLUE, Colors.WHITE, Colors.CYAN, Colors.WHITE));
 
-    private final String ircCode;
+    private final Function<String, String> formatter;
 
     public static Format fromString(String color) {
         final String enumValue = color.toUpperCase();
         return valueOf(enumValue);
     }
 
-    public String getIrcCode() {
-        return ircCode;
+    public String format(String string) {
+        return formatter.apply(string);
     }
 
     private Format(String ircCode) {
-        this.ircCode = ircCode;
+        this.formatter = applySingleColor(ircCode);
+    }
+
+    private Format(List<String> colors) {
+        this.formatter = applyMultipleColors(colors);
+    }
+
+    private static Function<String, String> applyMultipleColors(final List<String> colors) {
+        return new Function<String, String>() {
+            @Override
+            public String apply(String input) {
+                final Queue<String> colorQueue = newArrayDeque(colors);
+                final StringBuilder colorized = new StringBuilder(input.length() * 2);
+                for (char currentCharacter : input.toCharArray()) {
+                    final String color = colorQueue.remove();
+                    colorQueue.add(color);
+                    colorized.append(color).append(currentCharacter);
+                }
+                return colorized.toString();
+            }
+        };
+    }
+
+    private static Function<String, String> applySingleColor(final String color) {
+        return new Function<String, String>() {
+            @Override
+            public String apply(String input) {
+                return color + input;
+            }
+        };
     }
 }
