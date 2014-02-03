7f695a913ab2520617d69e40da9b35125b20ef2b
SL5R0 <warren@warren-gray.com>
2014-02-02 19:39:48 -0500

    - Updated formatted to clear formatting at the end of the formatted string.

--- src/test/java/com/sl5r0/qwobot/core/FormatTest.java
+++ src/test/java/com/sl5r0/qwobot/core/FormatTest.java
@@ -9,13 +9,13 @@ import static org.pircbotx.Colors.*;
 public class FormatTest {
     @Test
     public void ensureMultipleColorFormatsWork() throws Exception {
-        String rainbowize = Format.RAINBOW.format("message");
-        assertThat(rainbowize, equalTo(PURPLE + "m" + BLUE + "e" + CYAN + "s" + GREEN + "s" + YELLOW + "a" + RED + "g" + PURPLE + "e"));
+        final String rainbowize = Format.RAINBOW.format("message");
+        assertThat(rainbowize, equalTo(PURPLE + "m" + BLUE + "e" + CYAN + "s" + GREEN + "s" + YELLOW + "a" + RED + "g" + PURPLE + "e" + NORMAL));
     }
 
     @Test
     public void ensureSingleColorFormatsWork() throws Exception {
-        String rainbowize = Format.BLUE.format("message");
-        assertThat(rainbowize, equalTo(BLUE + "message"));
+        final String rainbowize = Format.BLUE.format("message");
+        assertThat(rainbowize, equalTo(BLUE + "message" + NORMAL));
     }
 }
