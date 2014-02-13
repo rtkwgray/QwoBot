ef987ee7f3c92562b50f95d4d324b293637da30f
Warren Gray <warren@warren-gray.com>
2014-02-13 13:13:41 -0500

    - Fixed PluginHelp so that it can actually be queried....

--- src/main/java/com/sl5r0/qwobot/plugins/help/ShowPluginHelp.java
+++ src/main/java/com/sl5r0/qwobot/plugins/help/ShowPluginHelp.java
@@ -1,7 +1,9 @@
 package com.sl5r0.qwobot.plugins.help;
 
 import com.google.api.client.repackaged.com.google.common.base.Joiner;
+import com.google.common.base.Function;
 import com.google.inject.Provider;
+import com.sl5r0.qwobot.plugins.Plugin;
 import com.sl5r0.qwobot.plugins.PluginManager;
 import com.sl5r0.qwobot.plugins.commands.Command;
 import com.sl5r0.qwobot.plugins.commands.ParameterizedTriggerCommand;
@@ -12,6 +14,7 @@ import org.pircbotx.hooks.events.MessageEvent;
 import java.util.List;
 import java.util.Set;
 
+import static com.google.common.collect.Iterables.transform;
 import static java.util.Collections.singletonList;
 
 public class ShowPluginHelp extends ParameterizedTriggerCommand {
@@ -19,7 +22,7 @@ public class ShowPluginHelp extends ParameterizedTriggerCommand {
     private final Provider<PluginManager> pluginManager;
 
     public ShowPluginHelp(Provider<PluginManager> pluginManager) {
-        super(TRIGGER, TO_LOWERCASE);
+        super(TRIGGER);
         this.pluginManager = pluginManager;
     }
 
@@ -47,6 +50,13 @@ public class ShowPluginHelp extends ParameterizedTriggerCommand {
 
     @Override
     public List<String> getHelp() {
-        return singletonList(TRIGGER + " <" + Joiner.on("|").join(pluginManager.get().getRegisteredPlugins()) + ">");
+        final Set<Plugin> registeredPlugins = pluginManager.get().getRegisteredPlugins();
+        final Iterable<String> pluginNames = transform(registeredPlugins, new Function<Plugin, String>() {
+            @Override
+            public String apply(Plugin plugin) {
+                return plugin.getName();
+            }
+        });
+        return singletonList(TRIGGER + " <" + Joiner.on("|").join(pluginNames) + ">");
     }
 }
